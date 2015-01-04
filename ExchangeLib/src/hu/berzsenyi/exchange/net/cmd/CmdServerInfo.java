b7c7f43c2899ad41a0f90f617d4f7447374ee51e
Balázs Fehér <fbalazs369@gmail.com>
2015-01-04 10:52:38 +0100

    Command changes

---
+++ ExchangeLib/src/hu/berzsenyi/exchange/net/cmd/CmdServerInfo.java
@@ -0,0 +1,38 @@
+package hu.berzsenyi.exchange.net.cmd;
+
+import hu.berzsenyi.exchange.Model;
+import hu.berzsenyi.exchange.Stock;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+
+public class CmdServerInfo extends TCPCommand {
+	public static final int ID = 3;
+	
+	public Stock[] stockList;
+	
+	public CmdServerInfo(int length) {
+		super(ID, length);
+	}
+	
+	public CmdServerInfo(Model model) {
+		super(ID, model.getStockCmdLength());
+		this.stockList = model.stockList;
+	}
+	
+	@Override
+	public void read(DataInputStream in) throws Exception {
+		this.stockList = new Stock[in.readInt()];
+		for(int s = 0; s < this.stockList.length; s++)
+			this.stockList[s] = new Stock(readString(in), in.readDouble());
+	}
+	
+	@Override
+	public void write(DataOutputStream out) throws Exception {
+		out.writeInt(this.stockList.length);
+		for(int s = 0; s < this.stockList.length; s++) {
+			writeString(out, this.stockList[s].name);
+			out.writeDouble(this.stockList[s].value);
+		}
+	}
+}
--- ExchangeLib/src/hu/berzsenyi/exchange/net/cmd/CmdServerStocks.java
+++ ExchangeLib/src/hu/berzsenyi/exchange/net/cmd/CmdServerStocks.java
@@ -1,42 +1,33 @@
 package hu.berzsenyi.exchange.net.cmd;
 
-import hu.berzsenyi.exchange.Model;
-import hu.berzsenyi.exchange.Stock;
-
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 
 public class CmdServerStocks extends TCPCommand {
-	public static final int ID = 3;
+	public static final int ID = 9;
 	
 	public double startMoney;
-	public Stock[] stockList;
+	public String clientID;
 	
 	public CmdServerStocks(int length) {
 		super(ID, length);
 	}
 	
-	public CmdServerStocks(Model model) {
-		super(ID, 8+model.getStockCmdLength());
-		this.startMoney = model.startMoney;
-		this.stockList = model.stockList;
+	public CmdServerStocks(double startMoney, String clientID) {
+		super(ID, 8+4+stringLength(clientID));
+		this.startMoney = startMoney;
+		this.clientID = clientID;
 	}
 	
 	@Override
 	public void read(DataInputStream in) throws Exception {
 		this.startMoney = in.readDouble();
-		this.stockList = new Stock[in.readInt()];
-		for(int s = 0; s < this.stockList.length; s++)
-			this.stockList[s] = new Stock(readString(in), in.readDouble());
+		this.clientID = readString(in);
 	}
 	
 	@Override
 	public void write(DataOutputStream out) throws Exception {
 		out.writeDouble(this.startMoney);
-		out.writeInt(this.stockList.length);
-		for(int s = 0; s < this.stockList.length; s++) {
-			writeString(out, this.stockList[s].name);
-			out.writeDouble(this.stockList[s].value);
-		}
+		writeString(out, this.clientID);
 	}
 }
