a757ab214727cc60bf2e1246646f5a3a0ae56b64
Bence Hornák <hornak.bence@gmail.com>
2015-02-25 23:35:33 +0100

    Material design is coming!

---
+++ ExchangeClient/src/hu/berzsenyi/exchange/client/debug/CardView.java
@@ -0,0 +1,14 @@
+package hu.berzsenyi.exchange.client.debug;
+
+import hu.berzsenyi.exchange.client.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CardView extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main_tab_stocks_card);
+	}
+}
