b6a654b44f1d8784867470d7ffb813ee306f2162
Bence Hornák <hornak.bence@gmail.com>
2015-02-26 15:46:28 +0100

    ActionBar and CardView

--- ExchangeClient/src/hu/berzsenyi/exchange/client/debug/CardView.java
+++ ExchangeClient/src/hu/berzsenyi/exchange/client/debug/CardView.java
@@ -1,14 +1,20 @@
 package hu.berzsenyi.exchange.client.debug;
 
 import hu.berzsenyi.exchange.client.R;
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarActivity;
 
-public class CardView extends Activity {
+public class CardView extends ActionBarActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main_tab_stocks_card);
+
+		ActionBar actionBar = getSupportActionBar();
+		actionBar.setDisplayOptions(ActionBar.DISPLAY_SHOW_HOME
+				| ActionBar.DISPLAY_SHOW_TITLE);
+		actionBar.setIcon(R.drawable.ic_launcher);
 	}
 }
