382897f740c190ada05689df391a44565daa068e
Bence Hornák <hornak.bence@gmail.com>
2015-03-07 12:05:45 +0100

    EventParser

---
+++ ExchangeServer/src/hu/berzsenyi/exchange/server/EventParser.java
@@ -0,0 +1,13 @@
+package hu.berzsenyi.exchange.server;
+
+import java.io.InputStream;
+
+import hu.berzsenyi.exchange.Event;
+
+public class EventParser {
+	
+	public static Event[] parseEvents(InputStream in) {
+		
+	}
+
+}
