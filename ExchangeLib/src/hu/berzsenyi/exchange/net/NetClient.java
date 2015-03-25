2e7fa7bbcefc5ad1d8f23c0c40d159864221b806
Bence Hornák <hornak.bence@gmail.com>
2015-03-26 00:16:30 +0100

    RecieveThread

--- ExchangeLib/src/hu/berzsenyi/exchange/net/NetClient.java
+++ ExchangeLib/src/hu/berzsenyi/exchange/net/NetClient.java
@@ -2,6 +2,7 @@ package hu.berzsenyi.exchange.net;
 
 import hu.berzsenyi.exchange.net.msg.Msg;
 
+import java.io.EOFException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.InetSocketAddress;
@@ -12,123 +13,125 @@ import java.util.Vector;
 public class NetClient {
 	public static interface INetClientListener {
 		public void onConnected(NetClient net);
+
 		public void onObjectReceived(NetClient net, Msg msg);
+
 		public void onClosed(NetClient net, Exception e);
 	}
-	
+
 	private class ThreadReceive extends Thread {
 		@Override
 		public void run() {
-			while(connected)
+			while (connected)
 				try {
-					if(0 < oin.available()) {
-						Msg msg = (Msg)oin.readObject();
-						for(INetClientListener listener : listeners)
-							listener.onObjectReceived(NetClient.this, msg);
-					}
-				} catch(Exception e) {
+					Msg msg = (Msg) oin.readObject();
+					for (INetClientListener listener : listeners)
+						listener.onObjectReceived(NetClient.this, msg);
+				} catch (EOFException e) { // Really nothing special
+				} catch (Exception e) {
 					e.printStackTrace();
 					close(e);
 				}
 		}
 	}
-	
+
 	private class ThreadConnect extends Thread {
 		@Override
 		public void run() {
 			try {
 				socket = new Socket();
-				socket.setSoTimeout(TIMEOUT);
-				socket.connect(serverAddr);
-				oin = new ObjectInputStream(socket.getInputStream());
+				socket.connect(serverAddr, TIMEOUT);
 				oout = new ObjectOutputStream(socket.getOutputStream());
+				oout.flush();
+				oin = new ObjectInputStream(socket.getInputStream());
 				connected = true;
 				connecting = false;
 				new ThreadReceive().start();
-				for(INetClientListener listener : listeners)
+				for (INetClientListener listener : listeners)
 					listener.onConnected(NetClient.this);
-			} catch(Exception e) {
+			} catch (Exception e) {
 				e.printStackTrace();
 				close(e);
 			}
 		}
 	}
+
 	private static final int TIMEOUT = 3000;
-	
+
 	private Socket socket = null;
 	private SocketAddress serverAddr = null;
 	private ObjectInputStream oin = null;
 	private ObjectOutputStream oout = null;
 	private boolean connected = false, connecting = false;
 	private Vector<INetClientListener> listeners = new Vector<>();
-	
+
 	public void addListener(INetClientListener listener) {
 		listeners.add(listener);
 	}
-	
+
 	public void removeListener(INetClientListener listener) {
 		listeners.remove(listener);
 	}
-	
+
 	public boolean isConnected() {
 		return connected;
 	}
-	
+
 	public boolean isConnecting() {
 		return connecting;
 	}
-	
+
 	public SocketAddress getServerAddr() {
 		return serverAddr;
 	}
-	
+
 	public void connect(String host, int port) {
-		if(connected || connecting)
+		if (connected || connecting)
 			close(new Exception("Network line is already opened!"));
 		connecting = true;
 		serverAddr = new InetSocketAddress(host, port);
 		new ThreadConnect().start();
 	}
-	
+
 	public void sendMsg(Msg msg) {
 		try {
 			oout.writeObject(msg);
 			oout.flush();
-		} catch(Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 			close(e);
 		}
 	}
-	
+
 	private void close(Exception e) {
-		if(!connected && !connecting)
+		if (!connected && !connecting)
 			return;
-		if(oin != null)
+		if (oin != null)
 			try {
 				oin.close();
 				oin = null;
-			} catch(Exception e1) {
+			} catch (Exception e1) {
 				e1.printStackTrace();
 			}
-		if(oout != null)
+		if (oout != null)
 			try {
 				oout.close();
 				oout = null;
-			} catch(Exception e1) {
+			} catch (Exception e1) {
 				e1.printStackTrace();
 			}
-		if(socket != null)
+		if (socket != null)
 			try {
 				socket.close();
 				socket = null;
-			} catch(Exception e1) {
+			} catch (Exception e1) {
 				e1.printStackTrace();
 			}
 		connected = connecting = false;
-		for(INetClientListener listener : listeners)
+		for (INetClientListener listener : listeners)
 			listener.onClosed(this, e);
 	}
-	
+
 	public void close() {
 		close(null);
 	}
--- ExchangeLib/src/hu/berzsenyi/exchange/net/NetServer.java
+++ ExchangeLib/src/hu/berzsenyi/exchange/net/NetServer.java
@@ -2,6 +2,7 @@ package hu.berzsenyi.exchange.net;
 
 import hu.berzsenyi.exchange.net.msg.Msg;
 
+import java.io.EOFException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
@@ -11,123 +12,132 @@ import java.util.Vector;
 public class NetServer {
 	public static interface INetServerListener {
 		public void onOpened(NetServer net);
+
 		public void onClientConnected(NetServer net, NetServerClient netClient);
-		public void onObjectReceived(NetServer net, NetServerClient netClient, Msg msg);
-		public void onClientClosed(NetServer net, NetServerClient netClient, Exception e);
+
+		public void onObjectReceived(NetServer net, NetServerClient netClient,
+				Msg msg);
+
+		public void onClientClosed(NetServer net, NetServerClient netClient,
+				Exception e);
+
 		public void onClosed(NetServer net, Exception e);
 	}
-	
+
 	public class NetServerClient {
 		private class ThreadReceive extends Thread {
 			@Override
 			public void run() {
-				while(connected)
+				while (connected)
 					try {
-						if(0 < oin.available()) {
-							Msg msg = (Msg)oin.readObject();
-							for(INetServerListener listener : listeners)
-								listener.onObjectReceived(NetServer.this, NetServerClient.this, msg);
-						}
-					} catch(Exception e) {
+						Msg msg = (Msg) oin.readObject();
+						for (INetServerListener listener : listeners)
+							listener.onObjectReceived(NetServer.this,
+									NetServerClient.this, msg);
+
+					} catch (EOFException e) { // Really nothing special
+					} catch (Exception e) {
 						e.printStackTrace();
 						close(e);
 					}
 			}
 		}
-		
+
 		private Socket socket = null;
 		private String id = null;
 		private ObjectInputStream oin = null;
 		private ObjectOutputStream oout = null;
 		private boolean connected = false;
-		
+
 		private NetServerClient(Socket socket) {
 			try {
 				connected = true;
 				this.socket = socket;
-				id = socket.getInetAddress().toString()+":"+socket.getPort();
+				id = socket.getInetAddress().toString() + ":"
+						+ socket.getPort();
 				oout = new ObjectOutputStream(socket.getOutputStream());
+				oout.flush();
 				oin = new ObjectInputStream(socket.getInputStream());
 				new ThreadReceive().start();
-			} catch(Exception e) {
+			} catch (Exception e) {
 				e.printStackTrace();
 				close(e);
 			}
 		}
-		
+
 		public boolean isConnected() {
 			return connected;
 		}
-		
+
 		public String getId() {
 			return id;
 		}
-		
+
 		public void sendMsg(Msg msg) {
 			try {
 				oout.writeObject(msg);
 				oout.flush();
-			} catch(Exception e) {
+			} catch (Exception e) {
 				e.printStackTrace();
 				close(e);
 			}
 		}
-		
+
 		public void close() {
 			close(null);
 		}
-		
+
 		private void close(Exception e) {
-			if(!connected)
+			if (!connected)
 				return;
-			if(oin != null)
+			if (oin != null)
 				try {
 					oin.close();
 					oin = null;
-				} catch(Exception e1) {
+				} catch (Exception e1) {
 					e1.printStackTrace();
 				}
-			if(oout != null)
+			if (oout != null)
 				try {
 					oout.close();
 					oout = null;
-				} catch(Exception e1) {
+				} catch (Exception e1) {
 					e1.printStackTrace();
 				}
-			if(socket != null)
+			if (socket != null)
 				try {
 					socket.close();
 					socket = null;
-				} catch(Exception e1) {
+				} catch (Exception e1) {
 					e1.printStackTrace();
 				}
 			connected = false;
-			for(INetServerListener listener : listeners)
+			for (INetServerListener listener : listeners)
 				listener.onClientClosed(NetServer.this, this, e);
 		}
 	}
-	
+
 	private class ThreadListen extends Thread {
 		@Override
 		public void run() {
-			while(opened)
+			while (opened)
 				try {
 					Socket socket = serverSocket.accept();
-					if(socket != null) {
+					if (socket != null) {
 						NetServerClient client = new NetServerClient(socket);
 						synchronized (clients) {
 							clients.add(client);
 						}
-						for(INetServerListener listener : listeners)
+						for (INetServerListener listener : listeners)
 							listener.onClientConnected(NetServer.this, client);
 					}
-				} catch(Exception e) {
+				} catch (Exception e) {
 					e.printStackTrace();
 					close(e);
 				}
 		}
 	}
-	
+
 	private class ThreadOpen extends Thread {
 		@Override
 		public void run() {
@@ -136,99 +146,99 @@ public class NetServer {
 				opened = true;
 				opening = false;
 				new ThreadListen().start();
-				for(INetServerListener listener : listeners)
+				for (INetServerListener listener : listeners)
 					listener.onOpened(NetServer.this);
-			} catch(Exception e) {
+			} catch (Exception e) {
 				e.printStackTrace();
 				close(e);
 			}
 		}
 	}
-	
+
 	private ServerSocket serverSocket = null;
 	private int port = -1;
 	private Vector<NetServerClient> clients = new Vector<>();
 	private boolean opened = false, opening = false;
 	private Vector<INetServerListener> listeners = new Vector<>();
-	
+
 	public boolean isOpened() {
 		return opened;
 	}
-	
+
 	public boolean isOpening() {
 		return opening;
 	}
-	
+
 	public int getPort() {
 		return this.port;
 	}
-	
+
 	public void addListener(INetServerListener listener) {
 		listeners.add(listener);
 	}
-	
+
 	public void removeListener(INetServerListener listener) {
 		listeners.remove(listener);
 	}
-	
+
 	public void open(int port) {
-		if(opened || opening)
+		if (opened || opening)
 			close(new Exception("Network line is already opened!"));
 		opening = true;
 		this.port = port;
 		new ThreadOpen().start();
 	}
-	
+
 	public void sendMsgToXY(Msg msg, String id) {
 		synchronized (clients) {
-			for(int i = 0; i < clients.size(); i++)
-				if(clients.get(i).id.equals(id)) {
+			for (int i = 0; i < clients.size(); i++)
+				if (clients.get(i).id.equals(id)) {
 					clients.get(i).sendMsg(msg);
-					if(!clients.get(i).connected)
+					if (!clients.get(i).connected)
 						clients.remove(i--);
 				}
 		}
 	}
-	
+
 	public void sendMsgToAllExceptXY(Msg msg, String id) {
 		synchronized (clients) {
-			for(int i = 0; i < clients.size(); i++)
-				if(!clients.get(i).id.equals(id)) {
+			for (int i = 0; i < clients.size(); i++)
+				if (!clients.get(i).id.equals(id)) {
 					clients.get(i).sendMsg(msg);
-					if(!clients.get(i).connected)
+					if (!clients.get(i).connected)
 						clients.remove(i--);
 				}
 		}
 	}
-	
+
 	public void sendMsgToAll(Msg msg) {
 		synchronized (clients) {
-			for(int i = 0; i < clients.size(); i++) {
+			for (int i = 0; i < clients.size(); i++) {
 				clients.get(i).sendMsg(msg);
-				if(!clients.get(i).connected)
+				if (!clients.get(i).connected)
 					clients.remove(i--);
 			}
 		}
 	}
-	
+
 	private void close(Exception e) {
-		if(!opened && !opening)
+		if (!opened && !opening)
 			return;
-		for(NetServerClient client : clients)
+		for (NetServerClient client : clients)
 			client.close(e);
 		clients.clear();
-		if(serverSocket != null)
+		if (serverSocket != null)
 			try {
 				serverSocket.close();
 				serverSocket = null;
-			} catch(Exception e1) {
+			} catch (Exception e1) {
 				e1.printStackTrace();
 			}
 		opened = opening = false;
-		for(INetServerListener listener : listeners)
+		for (INetServerListener listener : listeners)
 			listener.onClosed(this, e);
 	}
-	
+
 	public void close() {
 		close(null);
 	}
